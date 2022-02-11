59de2f44b6b3fe32d7a5439b5c41dab7401f937e
Eric <eric36davis@icloud.com>
2022-02-11 16:33:50 +0100

    Add favorites filter to landmark list

--- Landmarks/View/LandmarkList.swift
+++ Landmarks/View/LandmarkList.swift
@@ -9,27 +9,36 @@
 import SwiftUI
 
 struct LandmarkList: View {
+   @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }
                 
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
             }
-            .navigationTitle("Landmarks")
+                .navigationTitle("Landmarks")
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        
-        ForEach(["iPhone SE (2nd generation)", "iPod touch (7th generation)"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-        }
+        LandmarkList()
     }
 }
